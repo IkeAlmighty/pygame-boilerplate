5b4476a4832033a778d2f2fe108acb8ee52a618d
Isaac Yates <isaac.c.yates-1@ou.edu>
2018-11-13 11:46:10 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This respository consists of boilerplate code to make it easier for me
+to organnize and get started with all my future pygame projects.
---
+++ engine.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Engine:
+
+    def __init__(self, screen_size, FULLSCREEN = 0):
+        self.screen_size = screen_size
+        
