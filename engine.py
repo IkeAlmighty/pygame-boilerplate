daf8432e3d1bf9bc44409a781ef7a9385a531e66
Isaac Yates <isaac.c.yates-1@ou.edu>
2018-11-15 16:22:36 -0600

    RenderableComponent requires update(), Button methods added

--- engine.py
+++ engine.py
@@ -23,7 +23,8 @@ game.start(screen_size=[800, 600], pygame.FULLSCREEN, )
 
 class Engine:
 
-    font = pygame.font.SysFont(font_name, font_size)
+    font = None # TODO: this is janky and bad coding aaaaaa
+    eventcache = None
 
     def start(self, screen_size, FULLSCREEN = 0, font_name='', font_size = 16):
         """This method is called to start the game. It sets up the game defaults
@@ -43,9 +44,13 @@ class Engine:
 
         os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 
+        Engine.eventcache = EventCache()
+
         self.__screen = pygame.display.set_mode(self.screen_size, FULLSCREEN)
         self.__render_queue = []
         
+        Engine.font = pygame.font.SysFont(font_name, font_size)# TODO: this is janky and bad coding aaaaaa
+
         self.fps = 60
         self.frame_count = 0
         self.running = True
@@ -80,22 +85,25 @@ class Engine:
         self.__render_queue.insert(0, renderable_component)
 
     def preload(self):
-        raise NotImplementedError("preload must overridden by subclass")
+        raise NotImplementedError("preload(self) must overridden by subclass")
     
     def loop(self):
-        raise NotImplementedError("loop must overridden by subclass")
+        raise NotImplementedError("loop(self) must overridden by subclass")
 
     def cleanup(self):
-        raise NotImplementedError("cleanup must overridden by subclass")
+        raise NotImplementedError("cleanup(self) must overridden by subclass")
 
 
 class RenderableComponent:
 
     def get_image(self):
-        raise NotImplementedError("get_image must be overridden by subclass")
+        raise NotImplementedError("get_image(self) must be overridden by subclass")
 
     def get_pos(self):
-        raise NotImplementedError("get_pos must be overriden by subclass")
+        raise NotImplementedError("get_pos(self) must be overriden by subclass")
+
+    def update(self):
+        raise NotImplementedError("update(self) must be overriden by subclass")
 
 class EventCache:
 
@@ -132,24 +140,47 @@ class EventCache:
     def mouse_long_pressed(self, button):
         return self.__mouse_buttons[button] and self.__mouse_buttons_lastframe[button]
 
-class Button(engine.RenderableComponent):
+class Button(RenderableComponent):
 
-    def __init__(self, topleft, text = None, image = None):
+    def __init__(self, topleft = (0, 0), text = None, image = None):
         if text is None and image is None:
             raise Exception("either text of image must be a non-none object")
-        
-        self.__topleft = topleft
 
         if image is not None:
-            self.image = image
-            self.rect = image.get_rect()
-            self.rect.move_ip(self.__topleft[0], self.__topleft[1])
+            self.__inactive_image = image
+            self.__active_image = self.__inactive_image.copy()
+            self.__active_image.set_alpha(200)
 
         elif text is not None:
-            self.image = Engine.font.render(text, False, (255, 255, 255), (0, 0, 0))
+            self.text = text #used in the update method
+            self.__inactive_image = Engine.font.render(text, False, (255, 255, 255), (0, 0, 0))
+            self.__active_image = Engine.font.render(text, False, (0, 0, 0), (255, 255, 255))
+        
+        self.__image = self.__inactive_image
+
+        self.rect = self.__image.get_rect()
+        self.rect.move_ip(topleft[0], topleft[1])
+
+        self.__pressed = False
 
     def get_image(self):
-        return self.image
+        return self.__image
 
     def get_pos(self):
-        return self.pos
+        return self.rect.topleft
+
+    def update(self):
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            if Engine.eventcache.mouse_long_pressed(0):
+                self.__image = self.__inactive_image
+            else:
+                self.__image = self.__active_image
+
+            if Engine.eventcache.mouse_released(0):
+                self.__pressed = True
+            else: self.__pressed = False
+        else: self.__image = self.__inactive_image
+
+    def is_pressed(self):
+        return self.__pressed
+
